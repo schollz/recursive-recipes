64913a342ee99562124b8aefe27e99f463de8f8e
Zack Scholl <zack.scholl@gmail.com>
2018-05-08 14:13:32 -0700

    update recipes

--- recipe/recipe.go
+++ recipe/recipe.go
@@ -5,17 +5,30 @@ type Recipes struct {
 }
 
 type Recipe struct {
-	Time string `toml:"time"`
+	// Hours it takes to create the products from reactants
+	Hours float64 `toml:"hours"`
 	// If it scales, then do not add time when changing volume
 	Scales     bool      `toml:"scales"`
 	Directions string    `toml:"directions"`
+	Notes      string    `toml:"notes"`
 	Product    []Element `toml:"product"`
 	Reactant   []Element `toml:"reactant"`
 }
 
 type Element struct {
-	Name string  `toml:"name"`
-	Cups float64 `toml:"cups"`
+	Name  string  `toml:"name"`
+	Cups  float64 `toml:"cups"`
+	Acres float64 `toml:"acres"`
+	Notes string  `toml:"notes"`
+}
+
+type SingleRecipe struct {
+	Product     string
+	Cups        float64
+	Hours       float64
+	Scales      bool
+	Directions  string
+	Ingredients []string
 }
 
 func Open(tomlfile string) (r Recipe, err error) {
--- recipe/recipe_test.go
+++ recipe/recipe_test.go
@@ -1,9 +1,6 @@
 package recipe
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"testing"
 
@@ -13,24 +10,9 @@ import (
 
 func TestOpen(t *testing.T) {
 	b, _ := ioutil.ReadFile("../recipes.toml")
-	fmt.Println(string(b))
 	var r Recipes
-	r.Recipes = []Recipe{{
-		Time:       "alskdjf",
-		Directions: " alskdjf",
-		Product: []Element{
-			{Name: "food", Cups: 0.5},
-		},
-	}}
-	fmt.Println(r)
-	var firstBuffer bytes.Buffer
-	e := toml.NewEncoder(&firstBuffer)
-	err := e.Encode(r)
+	_, err := toml.Decode(string(b), &r)
 	assert.Nil(t, err)
-	fmt.Println(firstBuffer.String())
-
-	_, err = toml.Decode(string(b), &r)
-	assert.Nil(t, err)
-	bJson, _ := json.MarshalIndent(r, "", " ")
-	fmt.Println(string(bJson))
+	// bJson, _ := json.MarshalIndent(r, "", " ")
+	// fmt.Println(string(bJson))
 }
