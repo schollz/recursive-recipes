6c03576a4decb313ce8542dc6cd0822a45776c87
Zack Scholl <zack.scholl@gmail.com>
2018-05-08 19:32:37 -0700

    seems to work

--- recipe/recipe.go
+++ recipe/recipe.go
@@ -1,5 +1,7 @@
 package recipe
 
+import "time"
+
 type Recipes struct {
 	Recipes []Recipe `toml:"recipe"`
 }
@@ -9,36 +11,36 @@ type Recipes struct {
 // lowest irreducible
 type Recipe struct {
 	// Hours it takes to create the products from reactants
-	Hours float64 `toml:"hours"`
+	Hours float64 `toml:"hours" json:"hours,omitempty"`
 	// If it scales, then do not add time when changing volume
-	Scales     bool      `toml:"scales"`
-	Directions string    `toml:"directions"`
-	Notes      string    `toml:"notes"`
-	Product    []Element `toml:"product"`
-	Reactant   []Element `toml:"reactant"`
+	Scales     bool      `toml:"scales" json:"scales,omitempty"`
+	Directions string    `toml:"directions" json:"directions,omitempty"`
+	Notes      string    `toml:"notes" json:"notes,omitempty"`
+	Product    []Element `toml:"product" json:"product,omitempty"`
+	Reactant   []Element `toml:"reactant" json:"reactant,omitempty"`
+
+	// LastUpdated is the year it was last updated
+	// (refers to the price)
+	LastUpdated time.Time `toml:"updated" json:"updated,omitempty"`
 }
 
 type Element struct {
 	// Name is the name of the product/reactant
-	Name string `toml:"name"`
+	Name string `toml:"name" json:"name,omitempty"`
 
 	// Amount is the amount
-	Amount float64 `toml:"amount"`
+	Amount float64 `toml:"amount" json:"amount,omitempty"`
 
 	// Measure is the type of measurement, either
 	// "grams" (weight), "cups" (volume), "acres" (sq ft),
 	// or "whole"
-	Measure string `toml:"measure"`
+	Measure string `toml:"measure" json:"measure,omitempty"`
 
 	// Price is the cost per amount+measure
-	Price float64 `toml:"price"`
-
-	// LastUpdated is the year it was last updated
-	// (refers to the price)
-	Year int64 `toml:"year"`
+	Price float64 `toml:"price" json:"price,omitempty"`
 
 	// Notes are for references
-	Notes string `toml:"notes"`
+	Notes string `toml:"notes" json:"notes,omitempty"`
 }
 
 type SingleRecipe struct {
